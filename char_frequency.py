59a13216eebceb2b7f64341cf1da2c4ca5b57608
lanayam <lanayam@csu.fullerton.edu>
2025-03-19 01:14:37 -0700

    Reads txt file & outputs Character Frequencies.

---
+++ char_frequency.py
@@ -0,0 +1,26 @@
+# File that reads .txt file & counts character frequencies
+from collections import Counter
+
+def count_char_freq(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        text = f.read()
+
+    frequency = Counter(text)
+
+    return frequency
+
+file_path = "testfile.txt"
+
+char_frequencies = count_char_freq(file_path)
+
+print("Character Frequencies:")
+
+for char, freq in char_frequencies.items():
+    if char == " ":
+        print(f"'SPACE': {freq}")
+
+    elif char == "\n":
+        print(f"'NEWLINE': {freq}")
+
+    else:
+        print(f"'{char}': {freq}")
